5c6b8231de6de01fb5e77a69938d577ea52fd638
Rogier Verkaik <rogierv@gmail.com>
2018-09-30 07:06:41 +0200

    Slightly updated

--- scripts/main.js
+++ scripts/main.js
@@ -26,7 +26,7 @@ const myHeading = document.querySelector('h1');
 function setUserName() {
   const myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
 }
 
 if (!localStorage.getItem('name')) {
